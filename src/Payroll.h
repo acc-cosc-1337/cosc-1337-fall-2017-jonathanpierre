a9513ce064e5174e576ce01ed95d3a811a702aa9
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-20 18:32:16 -0500

    added functions prototypes for payroll program

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,6 +1,29 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
+#include <string>
+#include <sstream>
+using namespace std;
+
+int validateMenuChoice(int menuChoice);
+bool getOvertimeStatus(int hours);
+int validateHours(int hours);
+double validateRate(double rate);
+int getOvertimeHours(int hours);
+int getRegularHours(int hours, int overtimeHours);
+double validateSalary(double salary);
+
+double getfIT(double gross, double FITTAXRATE);
+double getficasSSN(double grossPay, double SSTAXRATE);
+double getficaMED(double grossPay, double MEDICARETAXRATE);
+double getNetPay(double grossPay, double fedIncomeTax, double ficaSSN, double ficaMED);
+
+stringstream getStream(bool isHourlyEmployee, string name, int hours, int overtimeHours,
+	double rate, double overtimeRate, double regularPay,
+	double overtimePay, double grossPay, double ficaSSN, double ficaMED, double fedIncomeTax, double netPay);
+void printPayroll();
+#endif // PAYROLL_H_INCLUDED
+
 
 
 
