ca8e26c65cfbc2a99c1fb78b99858919cea3ea19
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-20 19:02:04 -0500

    initial commit for two payroll tests

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,1 +1,8 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+TEST(payrollTest, test_overtimeHours_function)
+    EXPECT_EQ(5, getOvertimeHours(45))
+  
+  TEST(payrollTest, test_overtimeHours_function)
+    EXPECT_EQ(40, getRegularHours(50))
