39b7c48ac770b6f5b5c01df681f8f0b71cd70e9e
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-20 19:24:45 -0500

    fixed brackets added colons

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,8 +1,9 @@
 #include "gtest/gtest.h"
 #include "Payroll.h"
 
-TEST(payrollTest, test_overtimeHours_function)
-    EXPECT_EQ(5, getOvertimeHours(45))
-  
-  TEST(payrollTest, test_overtimeHours_function)
-    EXPECT_EQ(40, getRegularHours(50))
+TEST(payrollTest, test_overtimeHours_function){
+    EXPECT_EQ(5, getOvertimeHours(45));
+}  
+ TEST(payrollTest, test_overtimeHours_function){
+    EXPECT_EQ(40, getRegularHours(50));
+ }
