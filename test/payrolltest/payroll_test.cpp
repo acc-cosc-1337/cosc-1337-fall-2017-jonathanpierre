b632ed63bfd556c0636fc540e89b75e1c89113eb
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-27 18:39:41 -0500

    updated payrollTest2 suite

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -8,14 +8,28 @@ TEST(payrollTest, test_overtimeHours_returns_correct){
     EXPECT_EQ(40, getRegularHours(50, 10));
  }
 
-Test(payrollTest2, test_GetFIT_returns_correct){
+TEST(payrollTest2, test_GetFIT_returns_positive){
     EXPECT_EQ(15, getFIT(100));
 }
 
-Test(payrollTest2, test_GetOvertimeStatus_for_overtime_true){
+TEST(payrollTest2, test_GetFIT_returns_negative){
+    EXPECT_EQ(-15, getFIT(-100));
+}
+
+TEST(payrollTest2, test_GetOvertimeStatus_for_overtime_true){
     EXPECT_EQ(true, getOvertimeStatus(45));
 }
 
-Test(payrollTest2, test_GetOvertimeStatus_for_overtime_false){
+TEST(payrollTest2, test_GetOvertimeStatus_for_overtime_false){
     EXPECT_EQ(false, getOvertimeStatus(35));
+}
+
+
+TEST(payrollTest2, test_getNetPay_for_positive){
+    EXPECT_EQ(175, getNetPay(100, 25, 25, 25));
+}
+
+
+TEST(payrollTest2, test_getNetPay_when_negative){
+    EXPECT_EQ(-100, getNetPay(-25, -25, -25, -25));
 }
