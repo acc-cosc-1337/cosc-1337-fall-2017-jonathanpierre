878e8e80c9afd89ce17d2b03dbae3fa739480c5b
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-27 18:51:31 -0500

    fixed payrolltest2 function call

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -8,12 +8,12 @@ TEST(payrollTest, test_overtimeHours_returns_correct){
     EXPECT_EQ(40, getRegularHours(50, 10));
  }
 
-TEST(payrollTest2, test_GetFIT_returns_positive){
-    EXPECT_EQ(15, getFIT(100));
+TEST(payrollTest2, test_GetfIT_returns_positive){
+    EXPECT_EQ(15, getfIT(100));
 }
 
 TEST(payrollTest2, test_GetFIT_returns_negative){
-    EXPECT_EQ(-15, getFIT(-100));
+    EXPECT_EQ(-15, getfIT(-100));
 }
 
 TEST(payrollTest2, test_GetOvertimeStatus_for_overtime_true){
