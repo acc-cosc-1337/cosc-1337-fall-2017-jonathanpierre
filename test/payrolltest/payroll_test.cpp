d17c448c177c0f1f86a103f3206b8740514786c9
jonathanpierre <31940644+jonathanpierre@users.noreply.github.com>
2017-09-27 19:01:03 -0500

    fixed payrolltest2 input values for getnetpay

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -26,10 +26,10 @@ TEST(payrollTest2, test_GetOvertimeStatus_for_overtime_false){
 
 
 TEST(payrollTest2, test_getNetPay_for_positive){
-    EXPECT_EQ(175, getNetPay(100, 25, 25, 25));
+    EXPECT_EQ(25, getNetPay(100, 25, 25, 25));
 }
 
 
-TEST(payrollTest2, test_getNetPay_when_negative){
-    EXPECT_EQ(-100, getNetPay(-25, -25, -25, -25));
+TEST(payrollTest2, test_getNetPay_when_0){
+    EXPECT_EQ(0, getNetPay(0, 0, 0, 0));
 }
